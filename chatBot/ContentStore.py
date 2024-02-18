239ee972664df9064efe616ff672307b597595c8
LachlanWhite <73977986+LachlanWhite@users.noreply.github.com>
2024-02-18 12:58:31 -0800

    Add files

---
+++ chatBot/ContentStore.py
@@ -0,0 +1,129 @@
+import os.path
+import requests
+import json
+
+class ContentStore:
+    entryDB = []
+
+    class Entity:
+        courseDescription = ""
+        vector = []
+
+        def __init__(self, s: str, v: []):
+            self.courseDescription = s
+            self.vector = v
+        
+        def toJSON(self):
+            return json.dumps(self, default=lambda o: o.__dict__, sort_keys = True, indent = 4)
+
+    #def __init__(self):
+        #self.readFile()
+
+    def readFile(self):
+        fileName = "vectorDB.json"
+
+        '''
+        if not os.path.isfile(fileName):
+            print("File not found")
+        else:
+            with open(fileName) as file:
+                content = file.read().splitlines()
+
+                for line in content:
+                    entryDB.append(line)
+        '''
+        with open(fileName, "r") as json_file:
+            #tempList = json.load(json_file)
+
+            self.entryDB = []
+
+            while True:
+                line = json_file.readline()
+
+                if not line:
+                    break
+
+                EntityE = self.Entity(**json.loads(line))
+
+
+
+
+    def writeFile(self):
+        fileName = "vectorDB.json"
+
+        '''
+        if not os.path.isfile(fileName):
+            print("File not found")
+
+        else:
+            file1 = open("vectorDB.txt")
+            for x in entryDB:
+                file1.writelines(x)
+
+            file1.close()
+        '''
+        
+        with open(fileName, "w") as json_file:
+            for x in self.entryDB:
+                temp = json.dumps(x.__dict__)
+                json_file.writelines(temp + "\n")
+        
+    def getVector(self, userInput):
+        modelType = "text-embedding-3-small"
+        query = userInput
+
+        url = "https://api.openai.com/v1/embeddings"
+
+        payload = json.dumps({
+        "model": modelType,
+        "input": query
+        })
+
+        #Need to hide Authorization key here \/
+
+        headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer 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',
+        'Cookie': '__cf_bm=MyXTuajJDs2sfXyQPOsLBrgeUEhYfuD_e9p9eyWCmT8-1707591857-1-AdAv1O+w3r+YesTnNNvqWEPSCbzMSkSUVn/FbneGZgfIRHfRrI/hJOtpFiIQN5NcL7i2UpYL2PRDotczLdF68Ko=; _cfuvid=OnX782w1UPhMSvrliOKLtv5U603FU9F_az9j33S5x7I-1707591545673-0-604800000'
+        }
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+
+        apiData = response.json()
+
+        return apiData.get("data")[0].get("embedding")
+    
+
+    def compareVector(self, vector1, vector2):
+        comparisonScore = 0
+
+        minRange = min(len(vector1), len(vector2))
+
+        for i in range(minRange):
+            comparisonScore += vector1[i] * vector2[i]
+        
+        return comparisonScore
+    
+    def addEntry(self, userInput):
+        vector = self.getVector(userInput)
+
+        entityE = self.Entity(userInput, vector)
+
+        self.entryDB.append(entityE)
+
+        self.entryDB.append(entityE)
+
+        self.writeFile()
+
+        self.readFile()
+
+C = ContentStore()
+C.addEntry("What color is the sky?")
+
+
+
+
+
+    
+
+
---
+++ chatBot/main.py
@@ -0,0 +1,67 @@
+# run
+# export OPENAI_API_KEY="KEYHERE"
+
+import os
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage, SystemMessage
+#from IPython.display import display
+import ipywidgets as widgets
+from transformers import GPT2TokenizerFast
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+
+os.environ["OPENAI_API_KEY"] = "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"
+
+chat = ChatOpenAI(openai_api_key="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")
+
+with open('courseCatalog.txt', "r") as file:
+    text = file.read()
+
+tokenizer = GPT2TokenizerFast.from_pretrained("gpt2")
+
+def count_tokens(text: str) -> int:
+    return len(tokenizer.encode(text))
+
+text_splitter = CharacterTextSplitter(
+    separator = "\n\n",
+    chunk_size = 512,
+    chunk_overlap = 0,
+    length_function = count_tokens,
+    is_separator_regex = False
+)
+
+chunks = text_splitter.create_documents([text])
+
+embeddings = OpenAIEmbeddings()
+
+db = FAISS.from_documents(chunks, embeddings)
+
+#INSERT QUESTION HERE
+
+chain = load_qa_chain(OpenAI(temperature = 0), chain_type = "stuff")
+
+query = "What will I learn in CSS 101?"
+docs = db.similarity_search(query)
+docs[0]
+
+chain.run(input_documents = docs, question = query)
+
+messages = [
+    SystemMessage(content="You're a helpful assistant tasked with helping users identify university course details and prerequisites."),
+    HumanMessage(content="What is your purpose?")
+]
+
+myGPT = ConversationalRetrievalChain.from_llm(OpenAI(temperature = 0.1), db.as_retriever())
+
+chat_history = []
+
+while query.lower() != 'exit':
+    print("Type EXIT to stop, otherwise ask a question.")
+    query = input()
+    result = myGPT({"question": query, "chat_history": chat_history})
+    chat_history.append(query, result['answer'])
+
+print("Exiting Application.")
+
