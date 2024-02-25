f349d58e52724cf042a40bfd2fa8f8799c8a7f24
LachlanWhite <lachy.white12@gmail.com>
2024-02-24 18:01:36 -0800

    updated with cli conversation prompt

--- chatBot/Interface.py
+++ chatBot/Interface.py
@@ -5,36 +5,43 @@ import ContentStore
 
 url = "https://api.openai.com/v1/chat/completions"
 
-query = "What prerequisite courses are required to take CSS 350?"
+query = input("Question:")
+
+conversation = []
 
 ContentS = ContentStore.ContentStore()
 
-payload = json.dumps({
-  "model": "gpt-3.5-turbo",
-  "messages": [
-    {
-      "role": "assistant",
-      "content": ContentS.createPrompt(query, 0.66)
-    },
-    {
-      "role": "user",
-      "content": query
-    }
-  ],
-  "temperature": 1,
-  "top_p": 1,
-  "n": 1,
-  "stream": False,
-  "max_tokens": 4096,
-  "presence_penalty": 0,
-  "frequency_penalty": 0
-})
-headers = {
-  'Content-Type': 'application/json',
-  'Accept': 'application/json',
-  'Authorization': os.getenv('AUTH_TOKEN')
-}
+while(query != 'exit'):
 
-response = requests.request("POST", url, headers=headers, data=payload)
+  payload = json.dumps({
+    "model": "gpt-3.5-turbo",
+    "messages": [
+      {
+        "role": "assistant",
+        "content": ContentS.createPrompt(query, 0.66)
+      },
+      {
+        "role": "user",
+        "content": query
+      }
+    ],
+    "temperature": 1,
+    "top_p": 1,
+    "n": 1,
+    "stream": False,
+    "max_tokens": 4096,
+    "presence_penalty": 0,
+    "frequency_penalty": 0
+  })
+  headers = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    'Authorization': os.getenv('AUTH_TOKEN')
+  }
 
-print(response.text)
+  response = requests.request("POST", url, headers=headers, data=payload)
+
+  print(response.text)
+
+  conversation += query
+  query = input("question:")
